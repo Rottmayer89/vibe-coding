b0e139e584f6af17a9a378bd28c1579d9b35a7cd
Rottmayer <rottmayer1989@gmail.com>
2025-05-08 07:17:11 +0330

    feat: Category controller,routes,requests,policy & pass category controller tests

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Actions\DeleteCategoryAction;
+use App\Actions\StoreCategoryAction;
+use App\Actions\UpdateCategoryAction;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+use App\Models\Category;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\View\View;
+
+class CategoryController extends Controller
+{
+    public function index(Request $request): View
+    {
+        $categories = $request->user()->categories;
+
+        return view('categories.index', compact('categories'));
+    }
+
+    public function create(): View
+    {
+        return view('categories.create');
+    }
+
+    public function store(StoreCategoryRequest $request, StoreCategoryAction $action): RedirectResponse
+    {
+        $user = $request->user();
+
+        $action->handle($user, $request->validated());
+
+        return redirect()->route('categories.index');
+    }
+
+    public function edit(Category $category): View
+    {
+        Gate::authorize('update', $category);
+
+        return view('categories.edit', compact('category'));
+    }
+
+    public function update(UpdateCategoryRequest $request, Category $category, UpdateCategoryAction $action): RedirectResponse
+    {
+        Gate::authorize('update', $category);
+
+        $action->handle($category, $request->validated());
+
+        return redirect()->route('categories.index');
+    }
+
+    public function destroy(Category $category, DeleteCategoryAction $action): RedirectResponse
+    {
+        Gate::authorize('delete', $category);
+
+        $action->handle($category);
+
+        return redirect()->route('categories.index');
+    }
+}
